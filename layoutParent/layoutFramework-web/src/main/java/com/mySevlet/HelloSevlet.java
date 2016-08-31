fa9260d9484b12c5e259af098d046ec844feea93
johnneyChiu <johnney_chiu@163.com>
2016-08-31 15:47:13 +0800

    first maven layout project

---
+++ layoutParent/layoutFramework-web/src/main/java/com/mySevlet/HelloSevlet.java
@@ -0,0 +1,26 @@
+package com.mySevlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloSevlet extends HttpServlet {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+
+	{
+
+		response.setContentType("text/html");
+
+		response.setStatus(HttpServletResponse.SC_OK);
+
+		response.getWriter().println("<h1>HelloServlet</h1>");
+
+		response.getWriter().println("session=" + request.getSession(true).getId());
+		
+		System.out.println(1111);
+
+	}
+}
